aaa2dbeb337e589d8cd1f5c3d51fffe5536e2c32
olawale o samson <smsnmicheal@gmail.com>
2021-08-22 09:35:05 +0100

    changes made on the hamburger menu.

--- dist/main.js
+++ dist/main.js
@@ -1,19 +1,26 @@
-const menuBtn   = document.querySelector('.hamburger');
-const menuItems = document.querySelectorAll('.ham') ;
-const burger    = document.querySelector('.fa-hamburger');
+const menuBtn      = document.querySelector('.menu-btn');
+const menu         = document.querySelector('.menu-box');
+const menuNav      = document.querySelector('.menu-nav');
+const navItems     = document.querySelectorAll('.nav-item');
 
-showMenu = false;
+let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
-function toggleMenu () {
+function toggleMenu() {
     if(!showMenu){
-        menuItems.forEach(item => item.classList.add('show'));
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
 
         showMenu = true;
     } else {
-        menuItems.forEach(item => item.classList.remove('show'));
-        
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
         showMenu = false;
     }
 }
