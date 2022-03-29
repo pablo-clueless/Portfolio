9bc8bc47b75f9be17b3d706394a69b42c6180677
Olawale O Samson <smsnmicheal@gmail.com>
2022-03-29 23:06:29 +0100

    removed redundant codes

--- portfolio/comps/HeaderBackground.js
+++ portfolio/comps/HeaderBackground.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { OrbitControls, Stars } from "@react-three/drei"
 
-import Torus from "./Torus"
-import Sphere from "./Sphere"
 
 const HeaderBackground = () => {
     const [position, setPosition] = useState(0)
@@ -25,13 +23,8 @@ const HeaderBackground = () => {
             {/* directional light */}
             <directionalLight position={[0, 10, 0]} intensity={1.5}  castShadow />
 
-            {/* point lights */}
-            <pointLight position={[-10, 0, -20]} intensity={0.5} />
-            <pointLight position={[0, -10, 0]} intensity={1.5} />
-
             <group>
                 <Stars radius={100} depth={50} count={10000} factor={4} saturation={1} fade />
-                <Sphere position={[1, (position * -0.25), (position - 3)]} args={[1, 64, 64]} color="#1A5846" />
             </group>
             <OrbitControls enableZoom={false} enableRotate enablePan autoRotate autoRotateSpeed={0.5} />
         </Canvas>
