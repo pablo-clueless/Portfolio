f7b4fb130eb98396273a424c6b2205c1b60a4780
Olawale O Samson <smsnmicheal@gmail.com>
2022-04-19 12:44:35 +0100

    added a new project

--- portfolio/comps/ProjectData.js
+++ portfolio/comps/ProjectData.js
@@ -1,6 +1,5 @@
 export const Data = [
     {
-        cover: '/images/red-velvet.jpg',
         name: 'BLUE FLAMINGO',
         text: 'Blue Flamingo is a fictional restaurant in the heart of Lagos. It is one of my favorites.',
         lang: 'reactjs',
@@ -8,23 +7,20 @@ export const Data = [
         repo: 'https://github.com/pablo-clueless/Blue-Flamingo'
     },
     // {
-    //     cover: '/images/music-player.jpg',
     //     name: 'MUSIC PLAYER',
     //     text:" I'm a huge fan of music so why not build a music player? With cool minimalist design and a dash of JavaScript, it looks and feels good.",
     //     lang: 'html5 | css3 | javascript',
     //     live: 'https://pablos-musicplayer.netlify.app',
     //     repo: 'https://github.com/pablo-clueless/Music-Player'
     // },
+    // {
+    //     name: 'TEXT TO SPEECH',
+    //     text: 'A simple text to speech app with multiple voice options, adjustable speech rate and pitch. Looking to make this one big.',
+    //     lang: 'html5 | sass | javascript | speech-api',
+    //     live: 'https://t2speech.netlify.app',
+    //     repo: 'https://github.com/pablo-clueless/text-to-speech'
+    // },
     {
-        cover: '/images/text-to-speech.jpg',
-        name: 'TEXT TO SPEECH',
-        text: 'A simple text to speech app with multiple voice options, adjustable speech rate and pitch. Looking to make this one big.',
-        lang: 'html5 | sass | javascript | speech-api',
-        live: 'https://t2speech.netlify.app',
-        repo: 'https://github.com/pablo-clueless/text-to-speech'
-    },
-    {
-        cover: '/images/weather-app.jpg',
         name: 'WEATHER APP',
         text: 'Check accurate weather for any location. Data includes temperature, weather description, windspeed and others.',
         lang: 'reactjs | openweatherapi',
@@ -32,7 +28,6 @@ export const Data = [
         repo: 'https://github.com/pablo-clueless/WeatherApp'
     },
     {
-        cover: "/images/let's-chat.jpg",
         name: 'INSOCIAL',
         text: 'inSocial is an image sharing paltform. Share, download and save images, and interact with other users.',
         lang: 'reactjs | tailwind | sanity',
@@ -40,7 +35,6 @@ export const Data = [
         repo: 'https://github.com/pablo-clueless/insocial'
     },
     {
-        cover: '/images/dictionary.jpg',
         name: 'DICTIONARY',
         text: 'A basic English dictionary with phonetics, pronounciation, and other related langauge semantics.',
         lang: 'reactjs | rapidapi',
@@ -48,11 +42,17 @@ export const Data = [
         repo: 'https://github.com/pablo-clueless/Dictionary'
     },
     {
-        cover: '/images/',
         name: 'AIGIS',
         text: 'A car verification portal that uses the identitypass api to verify car details using one of bvn, nin or phone number.',
         lang: 'reactjs | nodejs | identitypass api',
         live: 'https://aigisorg.netlify.app',
         repo: 'https://github.com/aegis-org'
+    },
+    {
+        name: "PABLO'S STORE",
+        text: "An e-commerce store built with chec-commerce and stripe.",
+        lang: 'reactjs | material ui | commercejs',
+        live: 'https://e-commerce-store-bice.vercel.app/',
+        repo: 'https://github.com/pablo-clueless/e-commerce-store'
     }
 ]
