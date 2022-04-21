8d809aa5e5297dab3589e587d4a977c73b5f1d80
Olawale O Samson <smsnmicheal@gmail.com>
2022-04-21 19:34:27 +0100

    made changes in meta data

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -12,13 +12,6 @@ import Work from '../comps/Work'
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Samson Okunola | Portfolio Site</title>
-      <meta name='author' content='Samson Okunola'/>
-      <meta name='description' content="Samson Okunola's portfolio site"/>
-      <meta name='description' content="Samson Okunola is a frontend developer and content writer."/>
-      <link rel='shortcut icon' href='/branding.png' />
-    </Head>
     <Header />
     <MobileMenu />
     <About />
