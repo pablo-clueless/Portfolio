5987a72bf37d90998b27338d1f900d6e00f6978e
Olawale O Samson <smsnmicheal@gmail.com>
2022-04-25 07:51:59 +0100

    added seo image

--- portfolio/pages/_document.js
+++ portfolio/pages/_document.js
@@ -7,23 +7,23 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <meta charSet='utf-8' />
-            {/*  Primary Meta Tags */}
-          <meta name="title" content="Okunola Samson's Portfolio Site"  />
-          <meta name="description" content="Samson Okunola is a frontend developer  who loves minimalist designs and 3D animations." />
+          {/* -- Primary Meta Tags -- */}
+          <meta name="title" content="Okunola Samson" />
+          <meta name="description" content="Samson Okunola is a frontend developer who loves minimalist designs, 3D animations and a Web3 enthusiast." />
 
-          {/* Open Graph / Facebook */}
+          {/* -- Open Graph / Facebook -- */}
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://okunolasamson.vercel.app/" />
-          <meta property="og:title" content="Okunola Samson's Portfolio Site" />
-          <meta property="og:description" content="Samson Okunola is a frontend developer  who loves minimalist designs and 3D animations." />
-          <meta property="og:image" content="/public/lockscreen.png" />
+          <meta property="og:title" content="Okunola Samson" />
+          <meta property="og:description" content="Samson Okunola is a frontend developer who loves minimalist designs, 3D animations and a Web3 enthusiast." />
+          <meta property="og:image" content="https://res.cloudinary.com/pabloclueless/image/upload/v1650869377/Pablo-Clueless_rtt8d9.png" />
 
-          {/* Twitter */}
+          {/* -- Twitter -- */}
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://okunolasamson.vercel.app/" />
-          <meta property="twitter:title" content="Okunola Samson's Portfolio Site" />
-          <meta property="twitter:description" content="Samson Okunola is a frontend developer  who loves minimalist designs and 3D animations." />
-          <meta property="twitter:image" content="/public/lockscreen.png" />
+          <meta property="twitter:title" content="Okunola Samson" />
+          <meta property="twitter:description" content="Samson Okunola is a frontend developer who loves minimalist designs, 3D animations and a Web3 enthusiast." />
+          <meta property="twitter:image" content="https://res.cloudinary.com/pabloclueless/image/upload/v1650869377/Pablo-Clueless_rtt8d9.png" />
         </Head>
         <body>
           <Main />
