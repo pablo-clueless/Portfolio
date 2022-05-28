52b14e98a38f108f1d88a6019691f702acb7ac40
Olawale O Samson <smsnmicheal@gmail.com>
2022-05-28 23:56:39 +0100

    made changes!!

--- portfolio/comps/Footer.jsx
+++ portfolio/comps/Footer.jsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import { motion } from 'framer-motion'
-import { FiChevronUp } from 'react-icons/fi'
 
 import styles from '../styles/Footer.module.css'
 
@@ -11,21 +10,7 @@ const Footer = () => {
 
             <Link href='#home' passHref>
                 <motion.a
-                animate={{
-                    color: "#1A5846",
-                    scale: 1
-                }}
-                whileHover={{
-                    color: "#39FF14",
-                    translateY: [-5, 0, -5],
-                    transition: {
-                        type: "spring",
-                        stiffness: 100,
-                        duration: 0.5,
-                        repeat: Infinity
-                    }
-                }}
-                >
+                animate={{ color: "var(--light)", scale: 1 }} whileHover={{color: "var(--pry)", translateY: [-5, 0, -5], transition: {type: "spring", stiffness: 100, duration: 0.5, repeat: Infinity}}}>
                     scroll to top &uarr;
                 </motion.a>
             </Link>
