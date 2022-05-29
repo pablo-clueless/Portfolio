8fe7fa8fc7fa01c58945d520476e426fb97f8efe
Olawale O Samson <smsnmicheal@gmail.com>
2022-05-29 19:04:18 +0100

    made changes!!

--- portfolio/comps/Card.jsx
+++ portfolio/comps/Card.jsx
@@ -10,30 +10,24 @@ const Card = ({name, lang, repo, live, image_dt, icon}) => {
     return (
         <div className={styles.card}>
             <Icon icon={icon} className={styles.card_icon} />
-            <div className={styles.card_image}>
-                <Image src={image_dt} alt={name} width={200} height={250} />
-            </div>
             <h2>{name}</h2>
             <p>{lang}</p>
-            
-            <div className={styles.card_info}>
-                <ul>
-                    <li>
-                        <Link href={repo} passHref>
-                            <a target='_blank' rel='noreferrer noopener'>
-                                <FiGithub />
-                            </a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href={live} passHref>
-                            <a target='_blank' rel='noreferrer noopener'>
-                                <FiGlobe />
-                            </a>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
+            <ul>
+                <li>
+                    <Link href={repo} passHref>
+                        <a target='_blank' rel='noreferrer noopener'>
+                            <FiGithub />
+                        </a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href={live} passHref>
+                        <a target='_blank' rel='noreferrer noopener'>
+                            <FiGlobe />
+                        </a>
+                    </Link>
+                </li>
+            </ul>
         </div>
     )
 }
