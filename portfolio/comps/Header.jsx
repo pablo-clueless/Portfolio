79d3ee43c3ecc436c687e09e848b580b493efe2e
Olawale O Samson <smsnmicheal@gmail.com>
2022-05-31 21:14:47 +0100

    changed logo

--- portfolio/comps/Header.jsx
+++ portfolio/comps/Header.jsx
@@ -5,8 +5,7 @@ import useDownloader from 'react-use-downloader'
 import { FiArrowRight } from 'react-icons/fi'
 
 import styles from '../styles/Header.module.css'
-import logo from '../public/branding.png'
-
+import logo from '../public/logo.png'
 
 const menu = ['home', 'about', 'works', 'skills', 'contact']
 
@@ -18,7 +17,7 @@ const Header = () => {
     return (
         <motion.header className={styles.header} initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 5 } }} id="home">
             <nav className={styles.nav}>
-                <Image src={logo} alt='Samson Okunola' width={50} height={50} priority />
+                <Image src={logo} alt='Samson Okunola' width={70} height={70} priority />
                 <ul>
                     {menu.map(item => (
                     <li key={item}>
