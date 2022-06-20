14abf94a1ed6ea3da7431d6b98b612e190f1acfc
Samson Okunola <smsnmicheal@gmail.com>
2022-06-20 14:19:22 +0100

    stle changes in about.css

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -10,9 +10,7 @@ const About = () => {
             <h1>About</h1>
             <span>Who is Okunola Samson ?</span>
             <div className='about'>
-                <motion.div className='image__container'>
-                    <img src={img} alt='Okunola Samson' />
-                </motion.div>
+                <img src={img} alt='Okunola Samson' className='about_image' />
                 <p>I&apos;m a Frontend Web Developer and Blockchain enthusiast based in Lagos, Nigeria. I love minimalist & cool designs and animations. I spend most of my time either writing new codes or reviewing previous ones, adhering to real applications implementation with the knowledge and understanding of web development concepts.
                 <br /> <br />
                 I like looking for solutions and creating a great user experience. I&apos;ve worked with other developers on projects and created several working SPAs and websites that are maintainable and scalable.<br /> <br />
