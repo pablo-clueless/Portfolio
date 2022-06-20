1458cfe3107fe644e5a59a652c5a6b55e910fdd4
Samson Okunola <smsnmicheal@gmail.com>
2022-06-20 11:06:40 +0100

    fixes in about section

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Atropos from 'atropos/react'
 import { motion } from 'framer-motion'
 import 'atropos/css'
 
@@ -12,11 +11,9 @@ const About = () => {
             <h1>About</h1>
             <span>Who is Okunola Samson ?</span>
             <div className='about'>
-                <Atropos className='my-atropos'>
-                    <motion.div className='image__container'>
-                        <img src={img} alt='Okunola Samson' />
-                    </motion.div>
-                </Atropos>
+                <motion.div className='image__container'>
+                    <img src={img} alt='Okunola Samson' />
+                </motion.div>
                 <p>I&apos;m a Frontend Web Developer and Blockchain enthusiast based in Lagos, Nigeria. I love minimalist & cool designs and animations. I spend most of my time either writing new codes or reviewing previous ones, adhering to real applications implementation with the knowledge and understanding of web development concepts.
                 <br /> <br />
                 I like looking for solutions and creating a great user experience. I&apos;ve worked with other developers on projects and created several working SPAs and websites that are maintainable and scalable.<br /> <br />
