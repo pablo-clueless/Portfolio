2ccd1ad6d137d83a5c0f9d46547571d0d0f4af67
Samson Okunola <smsnmicheal@gmail.com>
2022-07-23 10:12:57 +0100

    made changes

--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -8,7 +8,7 @@ const Experience = () => {
   return (
     <div className='flex flex-col items-center text-center bg-black px-6 py-10' id="about">
       <div className='mb-12 text-left'>
-        <p className='text-6xl font-black font-heading text-primary'>
+        <p className='md:text-6xl text-4xl font-black font-heading text-primary'>
           EXPERIENCE
         </p>
       </div>
--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className='flex sm:flex-row flex-col items-center justify-between p-4 bg-main-bg dark:bg-main-dark-bg'>
             <p className='text-base text-white'>
-                Designed and built by Okunola Samson - <span className='text-primary'>@pablo_clueless</span>
+                Designed and built by <span className='text-primary'>@pablo_clueless</span>
             </p>
             <a href='#home' className='text-white hover:text-primary duration-500 bounce'>
                 scroll to top &uarr;
