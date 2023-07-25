9c478d559694c695526dab4d19f43d1df0e5553e
Samson Okunola <smsnmicheal@gmail.com>
2023-07-25 01:23:49 +0100

    mod: loading spinner

--- portfolio/src/components/Loader.tsx
+++ portfolio/src/components/Loader.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 const Loader = () => {
- return (
-  <div className="w-screen h-screen flex items-center justify-center bg-gray-700 fixed top-0 left-0 !z-10">
-  </div>
- )
+  return (
+    <div className="w-screen h-screen grid place-items-center bg-white fixed top-0 left-0 !z-10">
+      <div className="w-[50px] aspect-[1/1] relative animate-spin">
+        <div className="w-[15px] aspect-[1/1] rounded-full bg-black absolute top-0 left-0"></div>
+        <div className="w-[15px] aspect-[1/1] rounded-full bg-black absolute top-0 right-0"></div>
+        <div className="w-[15px] aspect-[1/1] rounded-full bg-black absolute bottom-0 left-0"></div>
+        <div className="w-[15px] aspect-[1/1] rounded-full bg-black absolute bottom-0 right-0"></div>
+      </div>
+    </div>
+  )
 }
 
 export default Loader
--- portfolio/tailwind.config.cjs
+++ portfolio/tailwind.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       animation: {
         jiggle: 'jiggle 150ms cubic-bezier(0.25, 1, 0.3, 0.8)',
+        shrink: 'shrink 300ms cubic-bezier(0.25, 1, 0.3, 0.8)',
       },
       backgroundImage: {
         mountains: "url('/images/mountains.jpg')",
@@ -17,6 +18,11 @@ module.exports = {
           "50%": { transform: 'translateX(2px)' },
           "75%": { transform: 'translateX(-2px)' },
           "100%": { transform: 'translateX(0)' },
+        },
+        shrink: {
+          "0%": { transform: "scale(1)"},
+          "50%": { transform: "scale(0.9)"},
+          "100%": { transform: "scale(1)"},
         }
       }
     },
