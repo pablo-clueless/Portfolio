c73df638854a3f3d8247ef7e5ae3b66839ec3a32
Samson Okunola <smsnmicheal@gmail.com>
2023-08-05 01:01:20 +0100

    changed cursor color to show on dark background

--- portfolio/src/components/Footer.tsx
+++ portfolio/src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className='w-full px-2 md:px-20 py-5'>
       <div className='w-full flex flex-wrap items-center justify-between gap-5'>
-        <Logo />
+        <Logo size="text-6xl" color="text-white" />
         <div className='flex items-center gap-4'>
           {CONNECT.map((item, index) => (
             <a key={index} href={item.url} target='_blank' className='bg-gray-500 p-2 rounded hover:scale-110'>
